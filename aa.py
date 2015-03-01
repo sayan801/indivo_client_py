68d1eeea8b61b8b755252aeb830b86a5beab0b93
chandra <chandra.sengupta@mycuratio.com>
2015-03-01 19:55:34 +0000

    create record

--- aa.py
+++ aa.py
@@ -45,7 +45,30 @@ print "Indivo Version: %s"%content
 resp, content = client.session_create({'username' : user , 'password' : password})
 if resp['status'] != '200':
     raise Exception("Bad Status: %s"%resp['status'])
-print dict(item.split("=") for item in content.split("&"))	
+result =  dict(item.split("=") for item in content.split("&"))	
+account_id =  result['account_id']
+
+demographics =  '<Demographics xmlns="http://indivo.org/vocab/xml/documents#"><dateOfBirth>1939-11-15</dateOfBirth><gender>female</gender><email>rabin.mohan@gmail.com</email><Name><familyName>mohan</familyName><givenName>rabin</givenName></Name></Demographics>'
+res, content = api.record_create(body=demographics)
+    status = res['status']
+    
+    # success, parse XML and change owner to current user
+    if '200' == status:
+        tree = ET.fromstring(content or '<Record/>')
+        if tree is not None:
+            record_id = tree.attrib.get('id')
+            res, content = api.record_set_owner(record_id=record_id, body=account_id, content_type='text/plain')
+            status = res['status']
+            if '200' == status:
+                print "Added record to: %s "%user
+            else:
+                print "Failed to add record to: %s "%user
+        else:
+            print "Record id not found: %s "%user
+    else:
+        print "Failed Create record to: %s "%user
+
+
 #print "Session info: %s"%content
 #print "Session_info Response object looks like: %s"%resp
    
