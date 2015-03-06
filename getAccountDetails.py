a652289d785a41846ed4e337c5cb115dd887543e
unknown <sayan801@gmail.com>
2015-03-06 11:17:22 +0530

    get account details

--- getAccountDetails.py
+++ getAccountDetails.py
@@ -11,8 +11,8 @@ length = len(sys.argv)
 
 print "Parameters: %s"%length
 
-if length != 8 :
-    raise Exception("Usage: Python Filename Username password givenname familyname email gender dob")
+if length != 3 :
+    raise Exception("Usage: Python Filename Username password")
 
 user = sys.argv[1]
 
@@ -21,12 +21,6 @@ print "Indivo User: %s"%user
 password = sys.argv[2]
 
 print "Indivo Password: %s"%password
-
-givenname  = sys.argv[3]
-familyname  = sys.argv[4]
-email  = sys.argv[5]
-gender = sys.argv[6]
-dob = sys.argv[7]
 
 # Need to pass these in to the client
 SERVER_PARAMS = {"api_base": "http://login.mycuratio.com:8000",
@@ -58,36 +52,8 @@ account_id =  result['account_id']
 
 print "Account id %s "%account_id
 
-xmlStart = '<Demographics xmlns="http://indivo.org/vocab/xml/documents#">'
+resp, content = api.account_info(account_email = account_id)
 
-demographics =  "%s<dateOfBirth>%s</dateOfBirth><gender>%s</gender><email>%s</email><Name><familyName>%s</familyName><givenName>%s</givenName></Name></Demographics>"%(xmlStart,dob,gender,email,familyname,givenname)
-
-print "demographics :%s"%demographics
-
-res, content = client.record_create(body=demographics)
-status = res['status']
-
-print "Record Create info: %s"%content
-
-# success, parse XML and change owner to current user
-if '200' == status:
-	tree = ET.fromstring(content or '<Record/>')
-	if tree is not None:
-		record_id = tree.attrib.get('id')
-		print "Record id %s "%record_id
-		res, content = client.record_set_owner(record_id=record_id, body=account_id, content_type='text/plain')
-		status = res['status']
-		print "Record Set Owner info: %s"%content
-		if '200' == status:
-			print "Added record to: %s "%user
-		else:
-			print "Failed to add record to: %s "%user
-	else:
-		print "Record id not found: %s "%user
-else:
-	print "Failed Create record to: %s "%user
-
-
-#print "Session info: %s"%content
-#print "Session_info Response object looks like: %s"%resp
-   
+if resp['status'] != '200':
+    raise Exception("Bad Status: %s"%resp['status'])
+print "Account Details: %s"%content
