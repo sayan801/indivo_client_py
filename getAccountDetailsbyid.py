dddc13651a60b5017e871b919323c4dbc8d4c33a
chandra <chandra.sengupta@mycuratio.com>
2015-03-07 09:50:53 +0000

    get account details by id

--- getAccountDetailsbyid.py
+++ getAccountDetailsbyid.py
@@ -11,24 +11,24 @@ length = len(sys.argv)
 
 print "Parameters: %s"%length
 
-if length != 3 :
+if length != 2 :
     raise Exception("Usage: Python Filename Username password")
 
 user = sys.argv[1]
 
 print "Indivo User: %s"%user
 
-password = sys.argv[2]
+#password = sys.argv[2]
 
-print "Indivo Password: %s"%password
+#print "Indivo Password: %s"%password
 
 # Need to pass these in to the client
 SERVER_PARAMS = {"api_base": "http://login.mycuratio.com:8000",
                  "authorization_base": "http://login.mycuratio.com"}
-CONSUMER_PARAMS = {"consumer_key": "curatehealth",
-                   "consumer_secret": "curatehealth"}
-#CONSUMER_PARAMS = {"consumer_key": "indivoadmin",
-#                   "consumer_secret": "indivoadminsecret"}
+#CONSUMER_PARAMS = {"consumer_key": "curatehealth",
+#                  "consumer_secret": "curatehealth"}
+CONSUMER_PARAMS = {"consumer_key": "indivoadmin",
+                   "consumer_secret": "indivoadminsecret"}
 
 # If we already had a token (access token, request token, or session token), it should be formatted
 # like this. We won't use this in the example.
@@ -37,27 +37,27 @@ RESOURCE_TOKEN = {"oauth_token": "asdfdsfa",
 
 # Set up the client (with no token): two-legged oauth only
 client = IndivoClient(SERVER_PARAMS, CONSUMER_PARAMS)
-#, pha_email=CONSUMER_PARAMS["consumer_key"])
+#                      , pha_email=CONSUMER_PARAMS["consumer_key"])
 
 # make the get_version call, and print it out
-resp, content = client.get_version(body={'a':'b', 'c':'d'})
+#resp, content = client.get_version(body={'a':'b', 'c':'d'})
 #if resp['status'] != '200':
 #   raise Exception("Bad Status: %s"%resp['status'])
-print "Indivo Version: %s"%content
+#print "Indivo Version: %s"%content
 
 
-resp, content = client.session_create({'username' : user , 'password' : password})
+#resp, content = client.session_create({'username' : user , 'password' : password})
 #if resp['status'] != '200':
 #    raise Exception("Bad Status: %s"%resp['status'])
-print "resp %s "%resp
-print "content %s "%content
+#print "resp %s "%resp
+#print "content %s "%content
 
-result =  dict(item.split("=") for item in content.split("&"))	
-account_id =  result['account_id']
+#result =  dict(item.split("=") for item in content.split("&"))	
+#account_id =  result['account_id']
 
-print "Account id %s "%account_id
+#print "Account id %s "%account_id
 
-resp, content = client.account_info(account_email = account_id)
+resp, content = client.account_info(account_email = user)
 
 #if resp['status'] != '200':
 #    raise Exception("Bad Status: %s"%resp['status'])
