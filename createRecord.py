38e6573db2650c867297ba39c5fde6f8a1123380
unknown <sayan801@gmail.com>
2015-03-07 16:29:55 +0530

    test api running

--- createRecord.py
+++ createRecord.py
@@ -11,8 +11,8 @@ length = len(sys.argv)
 
 print "Parameters: %s"%length
 
-if length != 3 :
-    raise Exception("Usage: Python Filename Username password")
+if length != 8 :
+    raise Exception("Usage: Python Filename Username password givenname familyname email gender dob")
 
 user = sys.argv[1]
 
@@ -22,6 +22,11 @@ password = sys.argv[2]
 
 print "Indivo Password: %s"%password
 
+givenname  = sys.argv[3]
+familyname  = sys.argv[4]
+email  = sys.argv[5]
+gender = sys.argv[6]
+dob = sys.argv[7]
 
 # Need to pass these in to the client
 SERVER_PARAMS = {"api_base": "http://login.mycuratio.com:8000",
@@ -51,4 +56,38 @@ if resp['status'] != '200':
 result =  dict(item.split("=") for item in content.split("&"))	
 account_id =  result['account_id']
 
-print "%s "%account_id
+print "Account id %s "%account_id
+
+xmlStart = '<Demographics xmlns="http://indivo.org/vocab/xml/documents#">'
+
+demographics =  "%s<dateOfBirth>%s</dateOfBirth><gender>%s</gender><email>%s</email><Name><familyName>%s</familyName><givenName>%s</givenName></Name></Demographics>"%(xmlStart,dob,gender,email,familyname,givenname)
+
+print "demographics :%s"%demographics
+
+res, content = client.record_create(body=demographics)
+status = res['status']
+
+print "Record Create info: %s"%content
+
+# success, parse XML and change owner to current user
+if '200' == status:
+	tree = ET.fromstring(content or '<Record/>')
+	if tree is not None:
+		record_id = tree.attrib.get('id')
+		print "Record id %s "%record_id
+		res, content = client.record_set_owner(record_id=record_id, body=account_id, content_type='text/plain')
+		status = res['status']
+		print "Record Set Owner info: %s"%content
+		if '200' == status:
+			print "Added record to: %s "%user
+		else:
+			print "Failed to add record to: %s "%user
+	else:
+		print "Record id not found: %s "%user
+else:
+	print "Failed Create record to: %s "%user
+
+
+#print "Session info: %s"%content
+#print "Session_info Response object looks like: %s"%resp
+   
